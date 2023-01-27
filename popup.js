dd3f30a1e52c690402db2aad5451cdc8416546b8
Angel Zhou <angel.zhou1016@gmail.com>
2023-01-27 17:05:26 -0500

    initial commit

---
+++ popup.js
@@ -0,0 +1,11 @@
+
+const saveButton = document.getElementById('save-btn')
+saveButton.addEventListener('click', event => {
+    event.preventDefault()
+    const column1 = document.getElementById('col-1').value
+    const column2 = document.getElementById('col-2').value
+    const column3 = document.getElementById('col-3').value
+    const column4 = document.getElementById('col-4').value
+    const column5 = document.getElementById('col-5').value
+    const column6 = document.getElementById('col-6').value
+})
