8866650fdaad27c91627bdff7491567dd67d7adf
Angel Zhou <angel.zhou1016@gmail.com>
2023-02-02 16:43:00 -0500

    updates onMessage listener that responds with getAuthToken on request

--- background.js
+++ background.js
@@ -1,13 +1,22 @@
-chrome.tabs.onUpdated.addListener(
-    function(tabId, changeInfo, tab) {
-      // read changeInfo data and do something with it
-      // like send the new url to contentscripts.js
-    //   if (changeInfo.url) {
-    //     chrome.tabs.sendMessage( tabId, {
-    //       message: 'hello!',
-    //       url: changeInfo.url
-    //     })
-    //   }
-    console.log('changed', changeInfo.url)
-    }
-  );
+// chrome.tabs.onUpdated.addListener(
+//     function(tabId, changeInfo, tab) {
+//       if (changeInfo.url) {
+//         chrome.tabs.sendMessage( tabId, {
+//           message: 'hello!',
+//           url: changeInfo.url
+//         })
+//       }
+//     console.log('changed', changeInfo.url)
+//     }
+//   );
+chrome.runtime.onMessage.addListener(
+    function (msg, sender, sendResponse) {
+        console.log("Received %o from %o, frame", msg, sender.tab, sender.frameId)
+        chrome.identity.getAuthToken({ interactive: true }, function (token) {
+            console.log('got the token', token)
+            sendResponse(`Gotcha! Here is the Token: ${token}`)
+        })
+        // As per this stackoverflow thread - https://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
+        // return true is needed to indicate that you'll call the response asynchronously
+        return true
+    })
