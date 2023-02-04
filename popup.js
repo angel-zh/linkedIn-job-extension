0b7a5ecafec91d3593c8055e2c2b4b536e980413
Angel Zhou <angel.zhou1016@gmail.com>
2023-02-04 09:51:41 -0500

    adds func to store form data in chrome storage api

--- popup.js
+++ popup.js
@@ -1,15 +1,13 @@
+const logIn = document.getElementById('log-in')
+const logOut = document.getElementById('log-out')
+const customColumns = document.querySelector('.custom-columns')
+const addColumnButton = document.getElementById('add-column-btn')
+const saveButton = document.getElementById('save-btn')
 
 let userToken = ""
+let counter = 1
 
-const logOut = document.getElementById('log-out')
-logOut.addEventListener('click', () => {
-  console.log(userToken)
-  window.fetch(`https://accounts.google.com/o/oauth2/revoke?token=${userToken}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-})
-
-const logIn = document.getElementById('log-in')
+// Log in and retrieve user token by sending message to background.js
 logIn.addEventListener('click', () => {
   chrome.runtime.sendMessage({ text: "Login Request from popup.js" }, function (response) {
     console.log("Response: ", response)
@@ -17,16 +15,24 @@ logIn.addEventListener('click', () => {
   })
 })
 
-const customColumns = document.querySelector('.custom-columns')
-let counter = 1
+// Log out does not work yet
+logOut.addEventListener('click', () => {
+  console.log(userToken)
+  window.fetch(`https://accounts.google.com/o/oauth2/revoke?token=${userToken}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+})
+
+// Add a custom column to the spreadsheet form
 function addColumn() {
   const input = document.createElement('input')
   const removeButton = document.createElement('button')
   const span = document.createElement('span')
-  input.setAttribute('type', 'text')
+  input.type = 'text'
+  input.setAttribute('name', `custom-col-${counter}`)
   input.classList.add('col')
   input.placeholder = 'Enter Column Title'
-  removeButton.innerText = '-'
+  removeButton.innerText = 'x'
   removeButton.onclick = removeColumn
   span.setAttribute('id', `custom-${counter}`)
   span.appendChild(input)
@@ -35,33 +41,31 @@ function addColumn() {
   counter += 1
 }
 
+// Remove selected column
 function removeColumn() {
   this.parentElement.remove()
 }
 
-const addColumnButton = document.getElementById('add-column-btn')
+function storeFormData() {
+  const form = document.getElementById('form')
+  const formData = new FormData(form)
+  const formObj = Object.fromEntries(formData)
+  chrome.storage.sync.set({ 'formObj': formObj }).then(() => {
+    console.log('stored')
+  })
+  // get result
+  //
+  // chrome.storage.sync.get(["formObj"]).then((result)=> {
+  //   console.log('result is ' + result.someKey)
+  // })
+  //
+  // => result is someValue
+}
 
 addColumnButton.addEventListener('click', event => {
   event.preventDefault()
   addColumn()
 })
-
-function storeFormData() {
-  const columnTitle1 = document.getElementById('col-1').value
-  localStorage.setItem('column1', columnTitle1)
-  const columnTitle2 = document.getElementById('col-2').value
-  localStorage.setItem('column2', columnTitle2)
-  const columnTitle3 = document.getElementById('col-3').value
-  localStorage.setItem('column3', columnTitle3)
-  const columnTitle4 = document.getElementById('col-4').value
-  localStorage.setItem('column4', columnTitle4)
-  const columnTitle5 = document.getElementById('col-5').value
-  localStorage.setItem('column5', columnTitle5)
-  const columnTitle6 = document.getElementById('col-6').value
-  localStorage.setItem('column6', columnTitle6)
-}
-
-const saveButton = document.getElementById('save-btn')
 
 saveButton.addEventListener('click', event => {
   event.preventDefault()
