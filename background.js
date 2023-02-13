640a3a049f0dae0504d870fbbda1aad78fc5fca5
Angel Zhou <angel.zhou1016@gmail.com>
2023-02-13 16:27:18 -0500

    declares isLoggedIn variable on retrieval of token, and store in chrome storage sync

--- background.js
+++ background.js
@@ -9,12 +9,18 @@
 //     console.log('changed', changeInfo.url)
 //     }
 //   );
+
 chrome.runtime.onMessage.addListener(
     function (msg, sender, sendResponse) {
         console.log("Received %o from %o, frame", msg, sender.tab, sender.frameId)
         chrome.identity.getAuthToken({ interactive: true }, function (token) {
             console.log('got the token', token)
-            sendResponse(`Response from background.js: ${token}`)
+            sendResponse(token)
+
+            let isLoggedIn = token ? true : false
+            chrome.storage.sync.set({ 'isLoggedIn': isLoggedIn }).then(() => {
+                console.log(`User is logged in? ${isLoggedIn}`)
+              })
         })
         // As per this stackoverflow thread - https://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
         // return true is needed to indicate that you'll call the response asynchronously
